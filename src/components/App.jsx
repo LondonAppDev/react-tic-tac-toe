68307e0b34233601aec70f082fcf6bdb0a7e2423
Mark Winterbottom <mark@londonappdeveloper.com>
2017-09-13 08:26:36 +0100

    Initial commit.

---
+++ src/components/App.jsx
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+import Game from './Game';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <h1>Naughts and Crosses</h1>
+        <Game />
+      </div>
+    );
+  }
+}
+
+export default App;
